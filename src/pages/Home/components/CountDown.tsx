e7bef309d3de300f664bd4aecf3725347bf8fc00
Vinicius Marinho <viniciusalexsander1@gmail.com>
2026-07-15 20:50:07 -0300

    feat: add countdown until next race

---
+++ src/pages/Home/components/CountDown.tsx
@@ -0,0 +1,41 @@
+import { Heading, Stack, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import type { RaceWeek } from "@/api/types/race";
+
+const COUNTDOWN_TIME = 1000;
+
+interface ICountDownProps {
+  nextRace: RaceWeek;
+}
+
+export const CountDown = ({ nextRace }: ICountDownProps) => {
+  const [countDown, setCountdown] = useState<number>(
+    Math.abs(
+      new Date(nextRace.dateStart + "T00:00:00").getTime() -
+        new Date().getTime(),
+    ),
+  );
+
+  useEffect(() => {
+    if (countDown === null) return;
+    const intervalId = setInterval(() => {
+      setCountdown((prevCountDown) => prevCountDown - COUNTDOWN_TIME);
+    }, COUNTDOWN_TIME);
+
+    return () => clearInterval(intervalId);
+  }, [countDown]);
+
+  const seconds = Math.floor((countDown / 1000) % 60);
+  const minutes = Math.floor((countDown / (1000 * 60)) % 60);
+  const hours = Math.floor((countDown / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+
+  return (
+    <Stack>
+      <Heading size="4xl" color="fg.success">
+        {days}d {hours}h {minutes}m {seconds}s
+      </Heading>
+      <Text color="fg.muted">até a próxima corrida</Text>
+    </Stack>
+  );
+};
